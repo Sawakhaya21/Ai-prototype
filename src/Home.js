60c59c393fc493c2ab78f7aaa49ad6c95fd41895
INGABIRE Landry <machinelab2021@gmail.com>
2025-01-23 16:50:55 +0200

    My first AI project

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Home() {
+
+    return (
+        <table className="dashboard-table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+export default Home;
